4d77a67eae43e235d6427f734ba19b607eb53fbe
adamiao <aadamiao@gmail.com>
2019-05-28 22:43:52 -0400

    Modification of python scripts

--- sobel_from_scratch.py
+++ sobel_from_scratch.py
@@ -30,18 +30,17 @@ input_image = imread(image_file)  # this is the array representation of the inpu
 # Extracting each one of the RGB components
 r_img, g_img, b_img = input_image[:, :, 0], input_image[:, :, 1], input_image[:, :, 2]
 
-# To make it grayscale_image we can use the following constants as weight
-# r_const, g_const, b_const = 0.2989, 0.5870, 0.1140
-# grayscale_image = r_const*r_img + g_const*g_img + b_const*b_img
-
 # The following operation will take weights and parameters to convert the color image to grayscale
 gamma = 1.343  # a parameter
 r_const, g_const, b_const = 0.2126, 0.7152, 0.0722  # weights for the RGB components respectively
 grayscale_image = r_const * r_img ** gamma + g_const * g_img ** gamma + b_const * b_img ** gamma
 
-# This command will display the grayscale image
-# plt.imshow(grayscale_image, cmap=plt.get_cmap('gray'))
-# plt.show()
+# This command will display the grayscale image alongside the original image
+fig1 = plt.figure(1)
+ax1, ax2 = fig1.add_subplot(121), fig1.add_subplot(122)
+ax1.imshow(input_image)
+ax2.imshow(grayscale_image, cmap=plt.get_cmap('gray'))
+fig1.show()
 
 # #------------------------------------------------------------------------------
 # PART II - Applying the Sobel operator
@@ -61,22 +60,25 @@ Gx = | 2.0   0.0  -2.0 |    and     Gy = |  0.0   0.0   0.0 |
 # Here we define the matrices associated with the Sobel filter
 Gx = np.array([[1.0, 0.0, -1.0], [2.0, 0.0, -2.0], [1.0, 0.0, -1.0]])
 Gy = np.array([[1.0, 2.0, 1.0], [0.0, 0.0, 0.0], [-1.0, -2.0, -1.0]])
-[rows, columns] = np.shape(grayscale_image)
-sobel_filtered_image = np.zeros(shape=(rows, columns))  # initialization of the output image array
+[rows, columns] = np.shape(grayscale_image)  # we need to know the shape of the input grayscale image
+sobel_filtered_image = np.zeros(shape=(rows, columns))  # initialization of the output image array (all elements are 0)
 
 # Now we "sweep" the image in both x and y directions and compute the output
 for i in range(rows - 2):
     for j in range(columns - 2):
-        s1 = np.sum(np.multiply(Gx, grayscale_image[i:i + 3, j:j + 3]))
-        s2 = np.sum(np.multiply(Gy, grayscale_image[i:i + 3, j:j + 3]))
-        sobel_filtered_image[i + 1, j + 1] = np.sqrt(s1 ** 2 + s2 ** 2)
+        s1 = np.sum(np.multiply(Gx, grayscale_image[i:i + 3, j:j + 3]))  # x direction
+        s2 = np.sum(np.multiply(Gy, grayscale_image[i:i + 3, j:j + 3]))  # y direction
+        sobel_filtered_image[i + 1, j + 1] = np.sqrt(s1 ** 2 + s2 ** 2)  # calculate the "hypotenuse"
 
-# Display the images
-fig = plt.figure()
-ax1, ax2 = fig.add_subplot(121), fig.add_subplot(122)
+# Display the original image and the Sobel filtered image
+fig2 = plt.figure(2)
+ax1, ax2 = fig2.add_subplot(121), fig2.add_subplot(122)
 ax1.imshow(input_image)
 ax2.imshow(sobel_filtered_image, cmap=plt.get_cmap('gray'))
-plt.show()
+fig2.show()
+
+# Show both images
+# plt.show()
 
 # Save the filtered image in destination path
 # plt.imsave('sobel_filtered_image.png', sobel_filtered_image, cmap=plt.get_cmap('gray'))
