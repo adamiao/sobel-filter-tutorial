6297689b2fbe70ee437b25444d84a5acfef8bc06
adamiao <aadamiao@gmail.com>
2019-05-31 01:08:25 -0400

    Modification of sobel_from_scratch.py

--- sobel_from_scratch.py
+++ sobel_from_scratch.py
@@ -18,9 +18,9 @@ from matplotlib.image import imread
 import matplotlib.pyplot as plt
 import numpy as np
 
-# #------------------------------------------------------------------------------
+# #---------------------------------------------------------------------------------------------------------------------
 # PART I - Transforming an image from color to grayscale
-# #------------------------------------------------------------------------------
+# #---------------------------------------------------------------------------------------------------------------------
 
 # Here we import the image file as an array of shape (nx, ny, nz)
 image_file = 'Images/original_image.PNG'
@@ -31,7 +31,7 @@ input_image = imread(image_file)  # this is the array representation of the inpu
 r_img, g_img, b_img = input_image[:, :, 0], input_image[:, :, 1], input_image[:, :, 2]
 
 # The following operation will take weights and parameters to convert the color image to grayscale
-gamma = 1.343  # a parameter
+gamma = 1.400  # a parameter
 r_const, g_const, b_const = 0.2126, 0.7152, 0.0722  # weights for the RGB components respectively
 grayscale_image = r_const * r_img ** gamma + g_const * g_img ** gamma + b_const * b_img ** gamma
 
@@ -42,9 +42,9 @@ ax1.imshow(input_image)
 ax2.imshow(grayscale_image, cmap=plt.get_cmap('gray'))
 fig1.show()
 
-# #------------------------------------------------------------------------------
+# #---------------------------------------------------------------------------------------------------------------------
 # PART II - Applying the Sobel operator
-# #------------------------------------------------------------------------------
+# #---------------------------------------------------------------------------------------------------------------------
 
 """
 The kernels Gx and Gy can be thought of as a differential operation in the "input_image" array in the directions x and y 
@@ -66,9 +66,9 @@ sobel_filtered_image = np.zeros(shape=(rows, columns))  # initialization of the 
 # Now we "sweep" the image in both x and y directions and compute the output
 for i in range(rows - 2):
     for j in range(columns - 2):
-        s1 = np.sum(np.multiply(Gx, grayscale_image[i:i + 3, j:j + 3]))  # x direction
-        s2 = np.sum(np.multiply(Gy, grayscale_image[i:i + 3, j:j + 3]))  # y direction
-        sobel_filtered_image[i + 1, j + 1] = np.sqrt(s1 ** 2 + s2 ** 2)  # calculate the "hypotenuse"
+        gx = np.sum(np.multiply(Gx, grayscale_image[i:i + 3, j:j + 3]))  # x direction
+        gy = np.sum(np.multiply(Gy, grayscale_image[i:i + 3, j:j + 3]))  # y direction
+        sobel_filtered_image[i + 1, j + 1] = np.sqrt(gx ** 2 + gy ** 2)  # calculate the "hypotenuse"
 
 # Display the original image and the Sobel filtered image
 fig2 = plt.figure(2)
